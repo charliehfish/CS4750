7d5bb7a424aa1c5d14e6edd05d19c21cf0223d41
Charles Fish <charliehfish@gmail.com>
2023-11-29 18:49:24 -0500

    Initial commit of connect-db.php

---
+++ connect-db.php
@@ -0,0 +1,24 @@
+<?php
+    $username = 'chf2pn'; 
+    $password = 'Fall2023';
+    $host = 'mysql01.cs.virginia.edu';
+    $dbname = 'chf2pn';
+    $dsn = "mysql:host=$host;dbname=$dbname";
+
+    try 
+    {
+        $db = new PDO($dsn, $username, $password);
+    
+    }
+    catch (PDOException $e) 
+    {
+    $error_message = $e->getMessage();        
+    echo "<p>An error occurred while connecting to the database: $error_message </p>";
+    }
+    catch (Exception $e)      
+    {
+    $error_message = $e->getMessage();
+    echo "<p>Error message: $error_message </p>";
+    }
+
+?>
