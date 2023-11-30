ebbf202276176ac3e9c049bc47b9b52904d0ca74
Charles Fish <charliehfish@gmail.com>
2023-11-29 19:03:29 -0500

    getNotes SQL statement

--- project-db.php
+++ project-db.php
@@ -1,5 +1,23 @@
 <?php
-    
+
+function getNotes($department, $course, $minRating) {
+    global $db;
+
+    $query = "select * from notes 
+              where department=:department 
+              AND course=:course 
+              AND rating>=:minRating";
+
+    $statement->bindValue(':department', $department);
+    $statement->bindValue(':course', $course);
+    $statement->bindValue(':minRating', $minRating);
+
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return $results;
+}
+
 
 
 
