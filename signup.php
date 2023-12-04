717ee3837365236e0c48e0e596d402532325ac74
pnavsingh <62856726+pnavsingh@users.noreply.github.com>
2023-12-03 22:14:46 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -15,33 +15,33 @@ if ($db === false) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $studentID = $_POST['studentID'];
     $email = $_POST['email'];
     $pswd = $_POST['pswd'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $year = $_POST['year'];
 
-    // Check if a user with the provided email already exists
-    $query = "SELECT * FROM Student WHERE email = ?";
+    $query = "SELECT * FROM Student WHERE email = ? AND pswd = ?";
     $stmt = $db->prepare($query);
-    $stmt->execute([$email]);
+    $stmt->execute([$email, $pswd]);
 
     if ($stmt->rowCount() > 0) {
-        echo "An account with this email already exists.";
+        // Start the session and redirect to user page
+        $_SESSION['email'] = $email;
+        header("Location: user.php");
+        exit;
     } else {
-        $query = "INSERT INTO Student (studentID, email, pswd, first_name, last_name, year) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = $db->prepare($query);
-        $stmt->execute([$studentID, $email, $pswd, $first_name, $last_name, $year]);
+        echo "Invalid email or password.";
     }
-
-    header("Location: homepage.php");
-    exit();
 }
 ?>
 
-<form action="signup.php" method="post">
-    Student ID: <input type="text" name="studentID" maxlength="15"><br>
+<form action="login.php" method="post">
     Email: <input type="email" name="email" maxlength="255"><br>
     Password: <input type="password" name="pswd" maxlength="255"><br>
-    First Name: <
+    <input type="submit" value="Log In">
+</form>
+
+<!-- Add a logout button -->
+<?php if (isset($_SESSION['email'])): ?>
+    <form action="logout.php" method="post">
+        <input type="submit" value="Log Out">
+    </form>
+<?php endif; ?>
