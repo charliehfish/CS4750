d9de7ed1623d669928fefd7444206b09ee6702af
pnavsingh <62856726+pnavsingh@users.noreply.github.com>
2023-12-03 22:11:18 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 
+// Store a message in the session
+$_SESSION['message'] = "You have been logged out.";
+
 // Destroy the session to log the user out and redirect them to the login page
 if(session_destroy()) {
-    header("Location: login_form.php");
+    header("Location: login.php");
 }
 ?>
