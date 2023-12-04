bfab1e0d0fd2791712f8818f60683e52c153e146
pnavsingh <62856726+pnavsingh@users.noreply.github.com>
2023-12-03 19:24:23 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,33 +2,30 @@
 require("connect-db.php");
 
 // Check connection
-if($db === false){
+if ($db === false) {
     die("ERROR: Could not connect. " . $db->errorInfo()[2]);
 }
 
-// Retrieve email, username, last name, and password from form
-$email = $_POST['email'];
-$uname = $_POST['uname'];
-$lname = $_POST['lname'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $studentID = $_POST['studentID'];
+    $email = $_POST['email'];
+    $pswd = $_POST['pswd'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $year = $_POST['year'];
 
-// Query the database
-$sql = "SELECT * FROM users WHERE email = :email AND username = :uname AND last_name = :lname AND password = :password";
-$statement = $db->prepare($sql);
-$statement->bindValue(':email', $email);
-$statement->bindValue(':uname', $uname);
-$statement->bindValue(':lname', $lname);
-$statement->bindValue(':password', $password);
-$statement->execute();
-
-if($statement->rowCount() > 0){
-    // Start the session and redirect to a different page
-    session_start();
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $uname;
-    header('location: welcome.php');
-} else {
-    // Redirect back to the login page with an error message
-    header('location: login.php?error=Incorrect username or password.');
+    $query = "INSERT INTO users (studentID, email, pswd, first_name, last_name, year) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$studentID, $email, $pswd, $first_name, $last_name, $year]);
 }
 ?>
+
+<form action="signup.php" method="post">
+    Student ID: <input type="text" name="studentID" maxlength="15"><br>
+    Email: <input type="email" name="email" maxlength="255"><br>
+    Password: <input type="password" name="pswd" maxlength="255"><br>
+    First Name: <input type="text" name="first_name" maxlength="255"><br>
+    Last Name: <input type="text" name="last_name" maxlength="255"><br>
+    Year: <input type="number" name="year" min="1"><br>
+    <input type="submit" value="Sign Up">
+</form>
